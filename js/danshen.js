fb8c7fb263b0da7d07def573643f273a6f799c05
aixing844 <844370295@qq.com>
2020-12-20 18:51:05 +0800

    liaixingweb

---
+++ js/danshen.js
@@ -0,0 +1,11 @@
+﻿ 
+function show() {
+    var danshen = document.getElementById("danshen");
+    danshen.style.display = "block"
+}
+ 
+function hide() {
+    var danshen = document.getElementById("danshen");
+    danshen.style.display = "none"
+
+}
